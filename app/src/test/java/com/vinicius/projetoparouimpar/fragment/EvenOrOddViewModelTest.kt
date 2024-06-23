8dab046d35a122777ebec92ccf8dcf8e034218fb
vinnybrito <viny.rbrito15@hotmail.com>
2024-06-22 21:20:13 -0300

    Adicionando Testes Unitários

--- app/src/test/java/com/vinicius/projetoparouimpar/fragment/EvenOrOddViewModelTest.kt
+++ app/src/test/java/com/vinicius/projetoparouimpar/fragment/EvenOrOddViewModelTest.kt
@@ -1,45 +1,32 @@
 package com.vinicius.projetoparouimpar.fragment
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.vinicius.projetoparouimpar.extensions.ZERO
 import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
-import org.junit.Rule
 import org.junit.Test
 
-@ExperimentalCoroutinesApi
 class EvenOrOddViewModelTest {
-
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private val viewModel by lazy { EvenOrOddViewModel() }
 
     @Test
-    fun givenCleanValues_thenResetValuesToZero() = runTest {
-        val resetedValue = Int.ZERO
-        viewModel.cleanValues()
-        assertEquals(resetedValue, viewModel.numericValue.value)
-    }
+    fun givenAddvalue_when() {
+        assertEquals(0, viewModel.numericValue.value)
 
-    @Test
-    fun `test evenOrOdd() with even number greater than or equal to 1`() = runTest {
         viewModel.addValue()
-        viewModel.evenOrOdd()
-        assertEquals("PAR", viewModel.resultValue.value)
+        assertEquals(1, viewModel.numericValue.value)
+
+        viewModel.addValue()
+        assertEquals(2, viewModel.numericValue.value)
+
+        viewModel.addValue()
+        assertEquals(3, viewModel.numericValue.value)
     }
 
     @Test
-    fun givenValueOnScreen_thenCheckIfValueIsEven() {
-        // Arrange
-        val displayedValue = 4
-        // Act
-        viewModel.evenOrOdd()
-
-        // Assert
-        assertEquals("PAR", viewModel.resultValue.value)
+    fun givenCleanValues_thenResetValuesToZero() {
+        viewModel.addValue()
+        viewModel.cleanValues()
+        assertEquals(0, viewModel.numericValue.value)
     }
-
 
 }
